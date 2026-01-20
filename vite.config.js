8179ef164b22272e444c482fd06f4abaa5cd590b
Nahueamado <nahuelamado.contacto@gmail.com>
2026-01-20 16:48:18 -0300

    Configuración PWA completada y Footer v1.9

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Plataforma de Árbitros',
+        short_name: 'Árbitros',
+        description: 'Gestión de arbitraje para Coodadef',
+        theme_color: '#000000', // Color de la barra superior en Android
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable' // Esto ayuda a que el ícono se vea bien en todos los modelos de Android
+          }
+        ]
+      }
+    })
+  ],
 })
